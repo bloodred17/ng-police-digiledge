73e2bd37407201e863abd005e9afc4fc8f316a96
Ankut Dutta <ankur.611@gmail.com>
2019-10-15 10:06:26 +0530

    button style

--- src/app/pipes/buttonforupdate.pipe.ts
+++ src/app/pipes/buttonforupdate.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ButtonforupdatePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: string, state: boolean): string {
+    return (state)? 'Done': 'Edit';
   }
 
 }
